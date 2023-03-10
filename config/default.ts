2076b970ba05ecbd2abd4b0779f05bdd624e0a3f
codebudy <a.shekhar5247@gmail.com>
2023-03-10 18:07:13 +0530

    initial setup

---
+++ config/default.ts
@@ -0,0 +1,12 @@
+export default {
+    port: 1337,
+    dbUri: "mongodb+srv://admin:VF75iM3av5ISgqq2@cluster0.ffj1wxi.mongodb.net/?retryWrites=true&w=majority",
+    // saltWorkFactor: 10,
+    // accessTokenTtl: "15m",
+    // refreshTokenTtl: "1y",
+    // accessTokenPrivateKey: ``,
+    // accessTokenPublicKey: ``,
+    // refreshTokenPrivateKey: ``,
+    // refreshTokenPublicKey: ``,
+  };
+  
